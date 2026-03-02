fdcb2e16e0a45fec79d0829a09f119bcaf21f49e
Slackz0r <gustav.aldenberg@gmail.com>
2026-03-02 12:44:59 +0100

    static data in getNavigation

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -13,5 +13,46 @@ export const getNavigation = async () => {
   }
 
   const data = await response.json();
-  return data;
+
+  const navigation = [
+    {
+      id: 1,
+      category: "Grundläggande information",
+      subCategory: [
+        { href: "/statutes", title: "Stadgar" },
+        { href: "/board", title: "Styrelse" },
+        { href: "/membership", title: "Medlemskap" },
+        { href: "/properties", title: "Fastighetsbeteckningar" },
+        { href: "/contact", title: "Kontakta oss" },
+      ],
+    },
+    {
+      id: 2,
+      category: "Verksamhetsinformation",
+      subCategory: [
+        { href: "/about", title: "Verksamhetsinformation" },
+        { href: "/news", title: "Händelser och projekt" },
+        { href: "/calendar", title: "Kalender" },
+        { href: "/reports", title: "Resultat och rapporter" },
+      ],
+    },
+    {
+      id: 3,
+      category: "Gemenskap",
+      subCategory: [
+        { href: "/gallery", title: "Bilder och video" },
+        { href: "/map", title: "Karta över området" },
+      ],
+    },
+    {
+      id: 4,
+      category: "Övrigt",
+      subCategory: [
+        { href: "/gdpr", title: "GDPR-information" },
+        { href: "/security", title: "Säkerhetsfunktioner" },
+        { href: "/links", title: "Länkar" },
+      ],
+    },
+  ];
+  return navigation;
 };
